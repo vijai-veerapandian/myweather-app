5faf9b5bd5cd19eca0ca0bb14d688c320a88bf66
vijai-veerapandian <vijai.veerapandian@gmail.com>
2025-03-25 18:03:47 -0400

    revised

--- src/api.js
+++
@@ -1,8 +0,0 @@
-export const GEO_API_URL = process.env.REACT_APP_GEO_API_URL;
-export const geoApiOptions = {
-    method: 'GET',
-    headers: {
-        'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
-        'x-rapidapi-host': process.env.REACT_APP_RAPIDAPI_HOST
-    }
-};
